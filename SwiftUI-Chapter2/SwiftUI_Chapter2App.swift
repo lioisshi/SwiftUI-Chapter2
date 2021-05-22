69de9e9aeecd830cc504183d519444651e2e7faf
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-05-22 13:38:49 +0900

    Initial Commit

---
+++ SwiftUI-Chapter2/SwiftUI_Chapter2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Chapter2App.swift
+//  SwiftUI-Chapter2
+//
+//  Created by Hiroaki Ishiga on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chapter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
